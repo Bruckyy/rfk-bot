f1705f2c9c64cc9010a28746bb5ccddd70ee7eba
LesTravaux <bouncebloc95@gmail.com>
2020-11-18 19:24:19 +0100

    duel upgrade

--- discod_bot.py
+++ discod_bot.py
@@ -6,32 +6,12 @@ from random import *
 bot= commands.Bot(command_prefix="$", description=":tools:")
 
 def rock_paper(p_1,p_2):
-    rock=2
-    paper=3
-    scissors=4
-    roll_1=randint(2,4)
-    roll_2=randint(2,4)
-    if roll_1==2 and roll_2==2:
-        result=":rock: Egalité :rock:"
-    elif roll_1==3 and roll_2==3:
-        result=":roll_of_paper: Egalité :roll_of_paper:"
-    elif roll_1==4 and roll_2==4:
-        result=":scissors: Egalité :scissors:"
-    elif roll_1==4 and roll_2==3:
-        result=f"{p_1} a gagné :scissors: :crossed_swords: :roll_of_paper:"
-    elif roll_1==2 and roll_2==3:
-        result= f"{p_2} a gagné :roll_of_paper: :crossed_swords: :rock:"
-    elif roll_1==2 and roll_2==4:
-        result= f"{p_1} a gagné :rock: :crossed_swords: :scissors:"
-    elif roll_2==2 and roll_1==3:
-        result= f"{p_1} a gagné :roll_of_paper: :crossed_swords: :rock:"
-    elif roll_2==2 and roll_1==4:
-        result= f"{p_2} a gagné :rock: :crossed_swords: :scissors:"
-    elif roll_2==4 and roll_1==3:
-        result=f"{p_2} a gagné :scissors: :crossed_swords: :roll_of_paper:"
-
+    roll=randint(1,2)
+    if roll==1:
+        result=f"{p_1} a Gagné !!"
+    else:
+        result=f"{p_2} a Gagné !!"
     return result
-
 
 @bot.event
 async def on_ready():
