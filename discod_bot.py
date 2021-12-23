5c4b6d78cd577edc4053f5c60e2019749ca3b5a9
LesTravaux <73838483+LesTravaux@users.noreply.github.com>
2021-12-23 18:44:34 +0100

    Update discod_bot.py

--- discod_bot.py
+++ discod_bot.py
@@ -198,7 +198,8 @@ async def t(ctx,*msg):
     message = " ".join(msg)
     f = open("model.txt", "r")
     if len(f.readlines())>=20:
-        f.write("The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Salut, qui est tu ?\nAI: Je suis une IA et mon prénom est RFK.\n")
+        with open('readme.txt', 'w') as f:
+            f.write("The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Salut, qui est tu ?\nAI: Je suis une IA et mon prénom est RFK.\n")
     f.close()
     f = open("model.txt", "r")
     training = "\n".join(f.readlines())
